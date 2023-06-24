d9d3b4df2a9c32e0720271b5e7a59943667cc8e6
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-24 11:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function getTime(){
+  let date=new Date();
+
+  let fullDate=date.toLocaleDateString();
+
+  let hh=date.getHours();
+  let sec=date.getSeconds();
+  let min=date.getMinutes();
+  let session="AM";
+
+  if(hh>12){
+    hh=hh-12;
+    session="PM";
+  }
+  hh = (hh==0) ? hh-12 : hh;
+
+  if(sec<10){sec="0"+sec}
+  if(min<10){min="0"+min}
+
+  let time=hh+":"+min+":"+sec+" "+session;
+
+  let dateTime=fullDate+", "+time;
+
+  document.getElementById("timer").innerText=dateTime;
+
+  setTimeout(function(){getTime()},1000);
+}
+
+getTime();
